ad7ea8eb11a8555a48bea18f0734fa667b78d3e2
Shai isenhower <shaiEisenhower1994@gmail.com>
2020-07-15 23:59:30 +0200

    added event details started

--- src/components/toy-details.cmp.vue
+++ src/components/toy-details.cmp.vue
@@ -1,34 +1,22 @@
 <template>
   <section>
-    <h3>Toy Details</h3>
-    <table border="1" v-if="toy">
-      <tr>
-        <th>id</th>
-        <th>name</th>
-        <th>price</th>
-        <th>type</th>
-        <td>Date Added</td>
-        <th>In Stock</th>
-      </tr>
-      <tr>
-        <td>
-          <h3>{{toy._id}}</h3>
-        </td>
-        <td>
-          <h3>{{toy.name}}</h3>
-        </td>
-        <td>
-          <h3>{{toy.price}}</h3>
-        </td>
-        <td>
-          <h3>{{toy.type}}</h3>
-        </td>
-        <td>{{toy.createdAt | moment("DD/MM/YYYY")}}</td>
-        <td>
-          <h3>{{toy.inStock}}</h3>
-        </td>
-      </tr>
-    </table>
+    <img
+      src="https://scontent.ftlv6-1.fna.fbcdn.net/v/t1.0-9/107255262_4175881475786223_6622631941189874568_o.jpg?_nc_cat=102&_nc_sid=340051&_nc_ohc=11dSkftZ8ccAX_dGn7d&_nc_ht=scontent.ftlv6-1.fna&oh=007935eb17fff4684d2e15e1b860d229&oe=5F35758E"
+    />
+    <div class="event-desc-container">
+      <h3>SATURDAY, JULY 18, 2020 AT 4 PM – 10 PM UTC+03</h3>
+      <h1>Find The love of your life on zoom : 18.9</h1>
+      <span>Online Event</span>
+    </div>
+
+    <div class="event-buttons-container">
+      <button>Interested</button>
+      <button>Going</button>
+      <button>Invite</button>
+      <button>copy</button>
+      <button>X</button>
+    </div>
+
     <el-button @click="back" class="btn-back">
       <div class="content">
         <i class="fas fa-arrow-left"></i>
@@ -36,40 +24,41 @@
       </div>
     </el-button>
 
-    <review-list v-if="toy"></review-list>
-    <chat-page v-if="toy" :toy="toy"></chat-page>
+    <!-- <review-list v-if="toy"></review-list>
+    <chat-page v-if="toy" :toy="toy"></chat-page> -->
   </section>
-</template>>
-        
+</template>
+>
+
 <script>
-import ToyService from "@/services/ToyService.js";
-import reviewList from "@/components/review-list.vue";
-import ChatPage from "@/views/ChatPage.vue";
+import ToyService from '@/services/ToyService.js'
+import reviewList from '@/components/review-list.vue'
+import ChatPage from '@/views/ChatPage.vue'
 
 export default {
-  name: "toy-details",
+  name: 'toy-details',
   components: {
     reviewList,
-    ChatPage
+    ChatPage,
   },
   data() {
     return {
-      toy: null
-    };
+      toy: null,
+    }
   },
   methods: {
     back() {
-      this.$router.push("/toy-app");
+      this.$router.push('/toy-app')
     },
     async loadToy() {
-      let toyId = this.$route.params.id;
-      this.toy = await ToyService.getById(toyId);
-    }
+      let toyId = this.$route.params.id
+      this.toy = await ToyService.getById(toyId)
+    },
   },
   created() {
-    this.loadToy();
-  }
-};
+    this.loadToy()
+  },
+}
 </script>
 
 <style lang="scss" scoped>
