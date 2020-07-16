7b5ead1142b2d971e935d9d8229b3bab8542d84c
shayelbaz1 <shaysha6@gmail.com>
2020-07-16 11:59:48 +0300

    S

--- src/components/toy-list.cmp.vue
+++ src/components/toy-list.cmp.vue
@@ -32,24 +32,24 @@ export default {
 </script>
 
 <style lang="scss">
-// button.add {
-//   position: fixed;
-//   width: 60px;
-//   height: 60px;
-//   bottom: 40px;
-//   right: 40px;
-//   background-color: #409eff;
-//   color: #fff;
-//   border-radius: 50px;
-//   text-align: center;
-//   box-shadow: 2px 2px 3px #999;
-//   padding: 0;
-//   margin: 0;
-//   border: none;
-//   outline: none;
-//   z-index: 1;
-//   &:active {
-//     box-shadow: none;
-//   }
-// }
+button.add {
+  position: fixed;
+  width: 60px;
+  height: 60px;
+  bottom: 40px;
+  right: 40px;
+  background-color: #850000;
+  color: #fff;
+  border-radius: 50px;
+  text-align: center;
+  box-shadow: 2px 2px 3px rgb(0, 0, 0);
+  padding: 0;
+  margin: 0;
+  border: none;
+  outline: none;
+  z-index: 1;
+  &:active {
+    box-shadow: none;
+  }
+}
 </style>
