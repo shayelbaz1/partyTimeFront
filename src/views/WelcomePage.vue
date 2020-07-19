348f6e53bd1c90b0f3d12bf9ef086eee081cf8cc
shay <shaysha6@gmail.com>
2020-07-19 17:25:58 +0300

    S

---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image"></b-img>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
