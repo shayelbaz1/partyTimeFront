3088a95d2f46d9c70df36ff52d4eda8b2428fef3
shay <shaysha6@gmail.com>
2020-07-19 18:53:24 +0300

    S

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -1,12 +1,10 @@
 <template>
   <section class="welcome-page">
-    <div class="parallax"></div>
-
-    <div class="welcome-body" style="height:1000px;background-color:red;font-size:36px">
-      Scroll Up and Down this page to see the parallax scrolling effect.
-      This div is just here to enable scrolling.
-      Tip: Try to remove the background-attachment property to remove the scrolling effect.
+    <div class="parallax">
+      <h1>YOUR ULTIMATE NIGHTLIFE APP</h1>
     </div>
+
+    <div class="body"></div>
   </section>
 </template>
 
@@ -19,17 +17,28 @@ export default {
 <style lang="scss">
 .welcome-page {
   .parallax {
-    /* The image used */
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
+    h1 {
+      margin: 0;
+      padding: 0;
+      position: relative;
+      top: 100px;
+      font-size: 2rem;
+      font-family: Avenir Next, Helvetica Neue, Helvetica, Arial, sans-serif;
+    }
     background-image: url("../assets/welcome2.jpg");
-
-    /* Set a specific height */
-    min-height: 500px;
-
-    /* Create the parallax scrolling effect */
+    min-height: 100vh;
     background-attachment: fixed;
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
+  }
+  .body {
+    height: 1000px;
+    // background-color: red;
+    font-size: 36px;
   }
 }
 </style>
