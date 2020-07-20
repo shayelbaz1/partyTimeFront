992740c8e522bffc3e332a8072221207077b9471
Shai isenhower <shaiEisenhower1994@gmail.com>
2020-07-20 10:15:39 +0200

    m

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -33,7 +33,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
+        <b-navbar-nav align="right">
           <b-nav-item @click="routeTo('/')">Party App</b-nav-item>
           <b-nav-item @click="routeTo('/map')">Map</b-nav-item>
           <b-nav-item @click="routeTo('/login')">Login</b-nav-item>
@@ -44,19 +44,18 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
+          <!-- <b-nav-form>
             <b-form-input size="sm" class="mr-sm-2" placeholder="Enter Location"></b-form-input>
             <b-button size="sm" class="my-2 my-sm-0 search-btn" type="submit">Search</b-button>
-          </b-nav-form>
+          </b-nav-form> -->
 
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
+          <!-- <b-nav-item-dropdown right>
             <template v-slot:button-content>
               <em>User</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+          </b-nav-item-dropdown> -->
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
