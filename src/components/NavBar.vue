85c65cfd47b75565fb0e74f995186b59694a4c04
shayelbaz1 <shaysha6@gmail.com>
2020-07-21 17:19:05 +0300

    S

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,12 +14,12 @@
             <i class="fas fa-search"></i>
           </b-nav-item>
           <b-nav-item @click="routeTo('/party-app')">Explore</b-nav-item>
-          <!-- <b-nav-item @click="routeTo('/map')">Map</b-nav-item> -->
           <b-nav-item @click="routeTo('/login')">Login</b-nav-item>
           <b-nav-item @click="routeTo('/signup')">Signup</b-nav-item>
           <b-nav-item @click="routeTo('/reviews')">Reviews</b-nav-item>
           <b-nav-item @click="routeTo('/profile')">Profile</b-nav-item>
           <b-nav-item @click="routeTo('/party-app/edit')">Create +</b-nav-item>
+          <!-- <b-nav-item @click="routeTo('/map')">Map</b-nav-item> -->
           <!-- <b-nav-item @click="routeTo('/chat')">Chat</b-nav-item> -->
         </b-navbar-nav>
 
