b992b01ec3dd3dfc95ef4451daef92bab74a5c89
shayelbaz1 <shaysha6@gmail.com>
2020-07-26 16:31:00 +0300

    s

--- src/components/party-details.cmp.vue
+++ src/components/party-details.cmp.vue
@@ -32,6 +32,7 @@
       <button>
         <share-network :partyID="this.party._id"></share-network>
         <i class="fab fa-whatsapp"></i>
+        Share
       </button>
     </div>
 
@@ -84,11 +85,7 @@
             </td>
             <td class="txt">
               <div class="flex">
-                <p
-                  class="type"
-                  v-for="(type, idx) in party.extraData.partyTypes"
-                  :key="idx"
-                >{{ type }} |</p>
+                <p class="type" v-for="(type, idx) in party.extraData.partyTypes" :key="idx">{{ type }} |</p>
               </div>
               <p class="desc">Party Types</p>
             </td>
@@ -99,11 +96,7 @@
             </td>
             <td class="txt">
               <div class="flex">
-                <p
-                  class="type"
-                  v-for="(type, idx) in party.extraData.musicTypes"
-                  :key="idx"
-                >{{ type }} |</p>
+                <p class="type" v-for="(type, idx) in party.extraData.musicTypes" :key="idx">{{ type }} |</p>
               </div>
               <p class="desc">Music Types</p>
             </td>
@@ -126,21 +119,13 @@
         <div class="members">
           <p class="title">Going</p>
           <div class="members-img-container">
-            <members-pics
-              v-for="member in party.extraData.members"
-              :key="member._id"
-              :member="member"
-            ></members-pics>
+            <members-pics v-for="member in party.extraData.members" :key="member._id" :member="member"></members-pics>
           </div>
         </div>
         <div class="members">
           <p class="title">Likes</p>
           <div class="members-img-container">
-            <members-pics
-              v-for="member in party.extraData.likes"
-              :key="member._id"
-              :member="member"
-            ></members-pics>
+            <members-pics v-for="member in party.extraData.likes" :key="member._id" :member="member"></members-pics>
           </div>
         </div>
       </div>
